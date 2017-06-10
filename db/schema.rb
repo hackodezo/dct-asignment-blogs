0cc5a6059180b9f0007b30ecfd46a77096cb9ea8
Athul <athul@hackodezo.com>
2017-06-10 17:30:30 +0530

    adds added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170610115435) do
+ActiveRecord::Schema.define(version: 20170610115615) do
+
+  create_table "advertisers", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
