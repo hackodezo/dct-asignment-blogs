1abb3c2f25dd5c6f42b3f8fbf434345b46ac1352
Athul <athul@hackodezo.com>
2017-06-10 17:49:27 +0530

    subscriber added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170610115615) do
+ActiveRecord::Schema.define(version: 20170610121655) do
 
   create_table "advertisers", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 20170610115615) do
   end
 
   create_table "authors", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "subscribers", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
